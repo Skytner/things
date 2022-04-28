d94d9e4dcb42b740aa0a111e768b808663f52832
Skytner <82650898+Skytner@users.noreply.github.com>
2022-04-28 14:22:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+question = prompt("Как вас зовут? Пожалуйста используйте полное имя")
+const result = ['Петр']
+if (question == result){
+window.location.href = 'hi.html'}
+else{
+    window.location.href = 'error.html'
+}
